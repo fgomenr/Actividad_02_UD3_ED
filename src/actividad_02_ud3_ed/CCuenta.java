58e07d7b2c398696eaee750efb0c715876f12fa0
fgomenr <fgomenr2704@g.educaand.es>
2021-12-13 14:02:13 +0100

    FGE- Revision 2. Genera JAVADOC

--- src/actividad_02_ud3_ed/CCuenta.java
+++ src/actividad_02_ud3_ed/CCuenta.java
@@ -25,9 +25,9 @@ public class CCuenta {
    /**
     * 
     * @param nom Nombre del titular de la cuenta
-    * @param cue
-    * @param sal
-    * @param tipo 
+    * @param cue Numero de cuenta
+    * @param sal La cantidad de dinero en cuenta
+    * @param tipo Tipo de cuenta
     */
     public CCuenta (String nom, String cue, double sal, double tipo)
     {
@@ -38,25 +38,35 @@ public class CCuenta {
     }
   /**
    * 
-   * @param nom 
+   * @param nom Nombre del titular de la cuenta
    */
     public void asignarNombre(String nom)
     {
         nombre=nom;
     }
-   
+   /**
+    * 
+    * @return Nombre del titular de la cuenta 
+    */
     public String obtenerNombre()
     {
         return nombre;
     }
 
-    
+    /**
+     * 
+     * @return Saldo de la cuenta 
+     */
      public double estado ()
     {
         return saldo;
     }
 
-    
+    /**
+     * 
+     * @param cantidad Cantidad de dinero de la cuenta
+     * @throws Exception Lanza excepcion no se puede ingresar una cantidad negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0){
@@ -64,7 +74,11 @@ public class CCuenta {
         setSaldo(saldo + cantidad);
     }
 
-   
+   /**
+    * 
+    * @param cantidad Cantidad de dinero de la cuenta
+    * @throws Exception No se puede retirar una cantidad negativa o no hay suficiente saldo
+    */
     public void retirar (double cantidad) throws Exception
     {
         if (cantidad < 0){
@@ -73,28 +87,43 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");}
         setSaldo(saldo - cantidad);
     }
-    
+    /**
+     * 
+     * @return Cuenta del titular 
+     */
     public String obtenerCuenta ()
     {
         return cuenta;
     }
 
-  
+  /**
+   * 
+   * @param cuenta Cuenta del titular
+   */
   public void setCuenta(String cuenta) {
     this.cuenta = cuenta;
   }
 
-  
+  /**
+   * 
+   * @param saldo Saldo de la cuenta
+   */
   public void setSaldo(double saldo) {
     this.saldo = saldo;
   }
 
- 
+ /**
+  * 
+  * @return tipo de interes  
+  */
   public double getTipoInterés() {
     return tipoInterés;
   }
 
-  
+  /**
+   * 
+   * @param tipoInterés  tipo de interes 
+   */
   public void setTipoInterés(double tipoInterés) {
     this.tipoInterés = tipoInterés;
   }
